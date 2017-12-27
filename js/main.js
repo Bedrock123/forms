0424db38d9161a5d9a3192545949859dff3f3052
Mark Bedrosian <markbedrosian@Marks-Air.home>
2017-12-27 03:15:37 -0500

    sdf

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function($){
 	if( $('.floating-labels').length > 0 ) floatLabels();
-
 	function floatLabels() {
 		var inputFields = $('.floating-labels .cd-label').next();
 		inputFields.each(function(){
@@ -11,25 +10,36 @@ jQuery(document).ready(function($){
 				singleInput.prev('.cd-label').addClass('float')
 				if (singleInput.val() != '') {
 					singleInput.removeClass('error');
+					singleInput.prev('.cd-label').removeClass('label-error');
+				}
+			});
+			$(window).keyup(function (e) {
+				var code = (e.keyCode ? e.keyCode : e.which);
+				if (code == 9 && $('#detect:focus').length) {
+					inputField.prev('.cd-label').addClass('float');
 				}
 			});
 			$(singleInput).blur(function() {
 				checkVal(singleInput);
 				if (singleInput.val() == '') {
 					singleInput.addClass('error');
+					singleInput.prev('.cd-label').addClass('label-error');
 				}
 			});
-			singleInput.on('change keyup', function(){				
-				checkVal(singleInput);
-
+			singleInput.on('change keyup', function(e){	
+				var code = (e.keyCode ? e.keyCode : e.which);
+				if (code != 9 && code != 16) {
+					checkVal(singleInput);
+				}			
 			});
 		});
 	}
-
 	function checkVal(inputField) {
-		
-		( inputField.val() == '' ) ? inputField.prev('.cd-label').removeClass('float') : inputField.prev('.cd-label').addClass('float');
-		( inputField.val() == '' ) ? console.log('ho') : inputField.removeClass('error');
+		if(!$(inputField).is(":focus")) {
+			( inputField.val() == '' ) ? inputField.prev('.cd-label').removeClass('float') : inputField.prev('.cd-label').addClass('float');
+		} 
+		( inputField.val() == '' ) ? console.log('') : inputField.removeClass('error');
+		( inputField.val() == '' ) ? console.log('') : inputField.prev('.cd-label').removeClass('label-error');
 	}
 	
 });
