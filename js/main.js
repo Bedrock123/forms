52750272e5296f861c2ee82c6385ff2c51a908c3
Mark Bedrosian <markbedrosian@Marks-Air.home>
2017-12-27 11:57:01 -0500

    sdf

--- js/main.js
+++ js/main.js
@@ -2,29 +2,34 @@ jQuery(document).ready(function($){
 	// Triggers the float field per form item
 	if( $('.floating-labels').length > 0 ) floatLabels();
 	function floatLabels() {
-		var inputFields = $('.floating-labels .cd-label').next();
-		inputFields.each(function(){
+		var inputFields = $('.floating-labels .c-label').next();
+			inputFields.each(function(){
+		
 			var singleInput = $(this);
+			$(singleInput.prev()).click(function(){
+				$(singleInput).focus();
+			});
+			$(this).after( "<div class='error-box' />" );
 			checkVal(singleInput);
 			$(singleInput).focus(function() {
-				singleInput.prev('.cd-label').addClass('float')
+				singleInput.prev('.c-label').addClass('float')
 				if (singleInput.val() != '') {
 					// If a form field is focussed on then no text is entered, add error
 					singleInput.next('.error-box').removeClass('error');
-					singleInput.prev('.cd-label').removeClass('label-error');
+					singleInput.prev('.c-label').removeClass('label-error');
 				}
 			});
 			$(window).keyup(function (e) {
 				var code = (e.keyCode ? e.keyCode : e.which);
 				if (code == 9 && $('#detect:focus').length) {
-					inputField.prev('.cd-label').addClass('float');
+					inputField.prev('.c-label').addClass('float');
 				}
 			});
 			$(singleInput).blur(function() {
 				checkVal(singleInput);
 				if (singleInput.val() == '') {
 					singleInput.next('.error-box').addClass('error');
-					singleInput.prev('.cd-label').addClass('label-error');
+					singleInput.prev('.c-label').addClass('label-error');
 				}
 			});
 			singleInput.on('change keyup', function(e){	
@@ -37,29 +42,9 @@ jQuery(document).ready(function($){
 	}
 	function checkVal(inputField) {
 		if(!$(inputField).is(":focus")) {
-			( inputField.val() == '' ) ? inputField.prev('.cd-label').removeClass('float') : inputField.prev('.cd-label').addClass('float');
+			( inputField.val() == '' ) ? inputField.prev('.c-label').removeClass('float') : inputField.prev('.c-label').addClass('float');
 		} 
 		( inputField.val() == '' ) ? console.log('') : inputField.next('.error-box').removeClass('error');
-		( inputField.val() == '' ) ? console.log('') : inputField.prev('.cd-label').removeClass('label-error');
+		( inputField.val() == '' ) ? console.log('') : inputField.prev('.c-label').removeClass('label-error');
 	}
-	// Trigger Nice Select Menu On Focu
-	function targetNiceSelect() {
-		setTimeout(function () {
-			$('.nice-select').focus(function() {
-				$('.nice-select').trigger('click')
-			});
-			// If enter is pressed on form, deactive menu
-			$(window).keyup(function (e) {
-				var code = (e.keyCode ? e.keyCode : e.which);
-				$('option').click(function() {
-					$('body').trigger('click')
-				});
-				if(code==13 && $('.nice-select').is(":focus")) {
-					$('body').trigger('click')
-				}
-			});
-		}, 2000);
-	}
-	targetNiceSelect()
-
 });
